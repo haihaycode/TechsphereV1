a334be1e32c825f0360ce43e6b0f1ab43a549a54
haihaycode <kienthuc177@gmail.com>
2024-05-03 14:14:13 +0700

    v3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="" class="flex items-center space-x-3 rtl:space-x-reverse"> 
-        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+        <img src="/image/logo1.png" class="h-8" alt="logo" />
         <RouterLink class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" to="/">Techsphere</RouterLink> 
       </a>
       <button @click="toggleMenu" data-collapse-toggle="navbar-default" type="button"
--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -2,31 +2,40 @@
 
   <!-- <Button :disabled="isButtonDisabled" @click="handleButtonClick" :text="buttonText" :loading="loading" /> -->
 
-  <SkeletonCard :loading="loading"  />
+  <SkeletonCard :loading="loading" />
 
 
 
   <div class="grid sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-4  gap-4 mx-2 lg:mx-20 ">
-    <div v-for="(user, index) in userList" :key="index" class="max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor" viewBox="0 0 20 20">
-        <path
-          d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
-      </svg>
+
+    <div v-for="(user, index) in userList" :key="index"
+      class="max-w p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ user.username }}</h5>
+        <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
       </a>
-      <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">{{ user.email }}</p>
-      <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
-        See our guideline
-        <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-          fill="none" viewBox="0 0 18 18">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778" />
-        </svg>
-      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
+            acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
+          acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Read more
+          <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9" />
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
+
+
+
+
 
 
 
@@ -47,7 +56,7 @@ export default {
   data() {
     return {
       isButtonDisabled: false,
-      loading : true,
+      loading: true,
       buttonText: 'Đăng ký',
       userList: []
     };
@@ -64,11 +73,12 @@ export default {
     async loadUserList() {
       try {
         const response = await axios.get('https://techsphere-production.up.railway.app/api/users/');
+
         this.userList = response.data;
       } catch (error) {
         console.error('Error fetching data:', error);
-      }finally{
-        this.loading=  false;
+      } finally {
+        this.loading = false;
       }
     }
   }
