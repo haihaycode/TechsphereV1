c9e3d8a6378cd71b5adb3f202b6715d5b2a45ee3
haihaycode <kienthuc177@gmail.com>
2024-06-28 17:25:12 +0700

    hai

--- src/components/account/sidebar.vue
+++ src/components/account/sidebar.vue
@@ -66,16 +66,18 @@
               class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300">
               <ul class="pt-2 ps-2">
                 <li>
-                  <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                    href="/account/info">
+                  <router-link
+                    class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
+                    to="/account/info">
                     Update Information
-                  </a>
+                  </router-link>
                 </li>
                 <li>
-                  <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
-                    href="/account/forgetpassword">
+                  <router-link
+                    class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300"
+                    to="/account/forgetpassword">
                     Change Password
-                  </a>
+                  </router-link>
                 </li>
               </ul>
             </div>
